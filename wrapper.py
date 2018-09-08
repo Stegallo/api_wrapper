f91c3b2fe890fadd3623d03ae68d3a506b1c6c7e
Stegallo <stegallo@gmail.com>
2018-09-08 16:30:31 -0700

    reformat final print

--- wrapper.py
+++ wrapper.py
@@ -6,12 +6,16 @@ import requests
 
 from credentials import *
 
+# http://developers.strava.com/docs/reference/
 SERVER = "https://www.strava.com/"
 VERSION = "api/v3/"
 
 ENDPOINT = SERVER + VERSION
 HEADERS = {"Authorization":"Bearer {}".format(access_token)}
 URL = ENDPOINT + "athlete"
-RESPONSE = requests.get(URL, headers=HEADERS)
+RESPONSE = requests.get(URL, headers=HEADERS).json()
 
-print(RESPONSE.json())
+print("Athlete: {} {} - Id: {}".format(
+    RESPONSE['firstname'],
+    RESPONSE['lastname'],
+    RESPONSE['id']))
